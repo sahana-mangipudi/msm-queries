968b6cf6622b4660e50e824dbcccd1d135ecdfa1
sahana-mangipudi <smangipudi@uchicago.edu>
2025-02-18 08:04:13 +0000

    added oldest and youngest directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,11 +5,18 @@ class DirectorsController < ApplicationController
 
   def show
     the_id = params.fetch("the_id")
-
     matching_records = Director.where({ :id => the_id })
-
     @the_director = matching_records.at(0)
-
     render({ :template => "director_templates/details" })
+  end
+
+  def junior
+    @youngest_director = Director.order(:dob).last
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def senior
+    @eldest_director = Director.order(:dob).first
+    render({ :template => "director_templates/eldest" })
   end
 end
