8a43aa7bb7f39b9de462967f62340d1587897a3c
sahana-mangipudi <smangipudi@uchicago.edu>
2025-02-18 07:46:42 +0000

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/youngest", { :controller => "directors", :action => "junior" })
+
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
+
+  get("/actors", { :controller => "actors", :action => "index" })
+
+  get("/actors/:the_id", { :controller => "actors", :action => "show" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
 end
